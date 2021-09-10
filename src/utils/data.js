cb83cbd1a219182687d9bbac4442179c467224dd
marienjus <80213832+marienjus@users.noreply.github.com>
2021-09-10 15:53:09 +0300

    Create data.js

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+export const data = [
+	{
+		id: 1,
+		name: "Warm Chicken",
+		imgSrc: "https://sisijemimah.com/wp-content/uploads/2015/07/image70.jpg",
+		price: "14.00",
+	},
+	{
+		id: 2,
+		name: "Chicken and Fried Rice",
+		imgSrc:
+			"https://img-global.cpcdn.com/recipes/a3f5568bcc2d947e/680x482cq70/nigeria-fried-rice-recipe-main-photo.webp",
+		price: "54.00",
+	},
+	{
+		id: 3,
+		name: "Hamburger",
+		imgSrc:
+			"https://static.toiimg.com/thumb/83007695.cms?width=680&height=512&imgsize=1287698",
+		price: "12.00",
+	},
+	{
+		id: 4,
+		name: "Eforiro",
+		imgSrc:
+			"https://lowcarbafrica.com/wp-content/uploads/2019/08/Efo-Riro-Nigerian-Spinach-Stew-3.jpg",
+		price: "18.00",
+	},
+	{
+		id: 6,
+		name: "Banga Soup",
+		imgSrc: "https://allnigerianfoods.com/wp-content/uploads/banga-soup1.jpg",
+		price: "16.00",
+	},
+]
