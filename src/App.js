48ce6cd25051f93b6438945f3e4e91c217840ded
marienjus <80213832+marienjus@users.noreply.github.com>
2021-09-10 15:45:57 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import Home from "./pages/home";
+import Cart from "./pages/cart";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<nav className="w-full h-20 px-20 shadow-xl flex justify-between items-center">
+				<Link
+					style={{ fontStyle: "oblique", color: "#FDF2F9" }}
+					className="text-lg bg-black py-1 px-2 tracking-widest logo"
+					to="/"
+				>
+					Foods App
+				</Link>
+
+				<NavLink
+					activeClassName="border-b-2"
+					className="tracking-wider"
+					to="/cart"
+					exact
+				>
+					Cart
+				</NavLink>
+			</nav>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/cart" component={Cart} />
+			</Switch>
+		</BrowserRouter>
+	);
+};
+
+export default App;
